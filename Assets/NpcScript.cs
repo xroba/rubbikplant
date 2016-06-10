ea0dbc26d18bb8a1002ebcee1bf96fead6de9baf
xavier roba <xroba@rubbik.com>
2016-06-10 14:29:25 +0200

    fine tuning

---
+++ Assets/NpcScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum NpcState { IDLE,TALK,MOVE,STAND }
+
+public class NpcScript : MonoBehaviour {
+    public float speedMove;
+    public bool MoveAnimation;
+    bool isTalking;
+    bool forceTalking;
+    NpcState currentNPCState;
+    Animator animator;
+    GameObject messagePanel;
+    Text messageText;
+
+	// Use this for initialization
+	void Start () {
+        currentNPCState = NpcState.IDLE;
+        animator = GetComponent<Animator>();
+
+        messagePanel = GameObject.Find("MessagePanel");
+        messageText = messagePanel.GetComponentInChildren<Text>();
+
+        messagePanel.SetActive(false);
+        isTalking = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (currentNPCState == NpcState.MOVE)
+        {
+            Debug.Log("Walk");
+            animator.SetBool("Stand", false);
+
+            animator.SetBool("Walk", true);
+            this.transform.position = new Vector3(this.transform.position.x + speedMove * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if(currentNPCState == NpcState.STAND)
+        {
+            Debug.Log("Stand");
+            animator.SetBool("Walk", false);
+
+            animator.SetBool("Stand", true);
+        }
+
+        if (currentNPCState == NpcState.TALK)
+        {
+            Debug.Log("Talk");
+            //first check that the buble is active
+            if (!messagePanel.activeSelf)
+            {
+                messagePanel.SetActive(true);
+            }
+
+            animator.SetBool("Stand", false);
+            animator.SetBool("Walk", false);
+
+            animator.SetTrigger("TalkTrigger");
+        }
+        
+	}
+
+
+    public void SetMove(float speedvalue)
+    {
+        speedMove = speedvalue;
+        currentNPCState = NpcState.MOVE;
+
+    }
+
+    public void setStand()
+    {
+        currentNPCState = NpcState.STAND;
+    }
+
+    public void setTalk(string msg)
+    {
+       
+            isTalking = true;
+            currentNPCState = NpcState.TALK;
+            messageText.text = msg;
+
+    }
+
+}
---
+++ Assets/PigBoss.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NpcScript))]
+public class PigBoss : MonoBehaviour {
+
+    public bool isMoving;
+    public float speedMove = 1;
+    public bool isStanding = false;
+    NpcScript npcScript;
+    bool persoMsg = false;
+    string[] PigBossSentence;
+	// Use this for initialization
+	void Start () {
+        npcScript = GetComponent<NpcScript>();
+        
+        PigBossSentence = new string[12];
+        PigBossSentence[0] = "Je m'ennuie";
+        PigBossSentence[1] = "...";
+        PigBossSentence[2] = "hmm, c'est toi ... bon vu ton niveau on est pas pret de terminer le jeu";
+        PigBossSentence[3] = "Qui touche un cactus se pique !";
+        PigBossSentence[4] = "Le pire con c'est le vieux con, on peut rien contre l'expérience.";
+        PigBossSentence[5] = "Moi, je ne vis que la nuit, depuis que quelqu'un m'a dit Un jour, tu vas mourir !";
+        PigBossSentence[6] = "Je me couche tellement tard et me leve tellement tôt que je me croise dans l'escalier ...";
+        PigBossSentence[7] = "Nath si tu vois un cochon clique dessus pour changer de musique";
+        PigBossSentence[8] = "Voir une araignée c'est rien, le pire c'est quand tu la voit plus .. !";
+        PigBossSentence[9] = "Ned Stark se marie avec la reine des dragons a la fin de la saison ... vrai ou pas ?";
+        PigBossSentence[10] = "Ton Score est de .... ah non on s en fou en fait";
+        PigBossSentence[11] = "je dois citer ma soeur Geraldine et mon frere Miguel ... juste une obligation familiale";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isMoving)
+        {
+            npcScript.SetMove(speedMove);
+        }
+
+        if(this.transform.position.x > 5 && isMoving)
+        {
+            isMoving = false;
+            isStanding = true;
+            npcScript.setStand();
+        }
+
+        if (isStanding)
+        {
+            Debug.Log("IsStanding = true");
+            isStanding = false;
+
+           // StartCoroutine(StartTalking());
+
+            //Invoke("StartTalking", 5);
+        }
+
+
+	}
+
+    IEnumerator StartTalking()
+    {
+        while (true)
+        {
+            retrieveSentenceAndTalk();
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+    public void retrieveSentenceAndTalk()
+    {
+        int rndValue = Random.Range(0, PigBossSentence.Length);
+        npcScript.setTalk(PigBossSentence[rndValue]);
+    }
+
+    public void StartForceTalking(string msg)
+    {
+        npcScript.setTalk(msg);
+    }
+
+}
